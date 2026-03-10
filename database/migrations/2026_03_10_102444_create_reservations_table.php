b34a70541b83184ceb5464b68718e5573b52761c
fadiinsaf <fadi_man_2005@outlook.fr>
2026-03-10 10:59:58 +0000

    feat: add reservations migrations

---
+++ database/migrations/2026_03_10_102444_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->foreignId("station_id");
+            $table->timestamp("start_time");
+            $table->integer("duration_minutes");
+            $table->timestamp("end_time");
+            $table->string("status");
+            $table->float("energy_delivered_kwh");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
